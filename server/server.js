f6a6569608205782a6ff5b5f746dacb322378861
Szymon Kasperczuk <kasperczukszymon2@gmail.com>
2026-06-07 22:40:57 +0200

    Add: Goal schema, step schema

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
 const connectDB = require('./config/database')
+const authRoutes = require('./routes/authRoutes')
 
 dotenv.config()
 connectDB()
@@ -12,6 +13,8 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', (req,res) => res.json({message:'api dziala'}))
+
+app.use('/api/auth',authRoutes)
 
 const PORT = process.env.PORT
 app.listen(PORT, ()=>console.log(`Server is running on port: ${PORT}`))
