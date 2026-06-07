d8ce96791fa3b3298077dba1ef0c27a8e4756c32
Szymon Kasperczuk <kasperczukszymon2@gmail.com>
2026-06-07 23:47:57 +0200

    Add: goal routes to app in server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv')
 const cors = require('cors')
 const connectDB = require('./config/database')
 const authRoutes = require('./routes/authRoutes')
+const goalRoutes = require('./routes/goalRoutes')
 
 dotenv.config()
 connectDB()
@@ -15,6 +16,7 @@ app.use(express.json())
 app.get('/', (req,res) => res.json({message:'api dziala'}))
 
 app.use('/api/auth',authRoutes)
+app.use('/api/goal',goalRoutes)
 
 const PORT = process.env.PORT
 app.listen(PORT, ()=>console.log(`Server is running on port: ${PORT}`))
